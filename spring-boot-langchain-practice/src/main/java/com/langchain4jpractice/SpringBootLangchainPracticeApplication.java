283faae2508a931c07dc1e72b3e01e861a7d92eb
Andres Quiroz <andresfqperez@gmail.com>
2026-02-07 02:13:51 -0500

    chore: initial Spring Boot project bootstrap from Spring Initializr

---
+++ spring-boot-langchain-practice/src/main/java/com/langchain4jpractice/SpringBootLangchainPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.langchain4jpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLangchainPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLangchainPracticeApplication.class, args);
+	}
+
+}
